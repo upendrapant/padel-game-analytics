52133535e09625420082a1717dcf3e0cd5f7fd16
upendrapant <pantupendra97@gmail.com>
2026-05-09 09:39:17 +0545

    feat: integrate ByteTrack, implement court ROI filtering, and display player counts in main visualizer

--- detector.py
+++ detector.py
@@ -2,13 +2,22 @@ from ultralytics import YOLO
 
 #Loads YOLOv8 model and performs object detection on a frame and returns detections list
 class PadelDetector:
-    def __init__(self, model_path="yolo11n.pt", conf=0.5, device='cuda',):
+    def __init__(self, model_path="yolo11m.pt", tracker="bytetrack.yaml", conf=0.5, device='cuda',):
         self.model = YOLO(model_path)
         self.conf = conf
         self.device = device
+        self.tracker = tracker
+        self.roi = {"x_min": 350, "x_max": 1450, "y_min": 10, "y_max": 880}
+    
+    def _is_in_court(self, bbox):
+        x1, y1, x2, y2 = bbox
+        cx = (x1 + x2) / 2
+        cy = y2 
+        return (self.roi["x_min"] < cx < self.roi["x_max"] and
+                self.roi["y_min"] < cy < self.roi["y_max"])
 
     def detect(self,frame):
-        results = self.model.track(frame, persist=True, classes=[0], conf=self.conf, device=self.device, verbose=False)
+        results = self.model.track(frame, persist=True, classes=[0], conf=self.conf, tracker=self.tracker, device=self.device, verbose=False)
         detections = []
         for r in results:
             if r.boxes is not None:
@@ -16,5 +25,7 @@ class PadelDetector:
                     x1, y1, x2, y2 = box.xyxy[0].tolist()
                     conf = box.conf[0].item()
                     track_id = box.id.item() if box.id is not None else None
-                    detections.append({"track_id": track_id, "bbox": [x1, y1, x2, y2], "class": "person", "conf": conf})
+                    if self._is_in_court([x1, y1, x2, y2]):
+                        detections.append({"track_id": track_id, "bbox": [x1, y1, x2, y2], "class": "person", "conf": conf})
+                        
         return detections
